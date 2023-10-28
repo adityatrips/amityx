fe587f44634ca6e1d7749073d5d7757298175c91
Aditya Tripathi <adityatripathi.at04@gmail.com>
2023-10-29 00:22:57 +0530

    changed cb url

--- app/login/google-btn.tsx
+++ app/login/google-btn.tsx
@@ -13,7 +13,7 @@ export default function GoogleButton() {
 		await supabase.auth.signInWithOAuth({
 			provider: 'google',
 			options: {
-				redirectTo: `${location.origin}/auth/gocallback`,
+				redirectTo: `${process.env.URL}/auth/gocallback`,
 			},
 		});
 	};
