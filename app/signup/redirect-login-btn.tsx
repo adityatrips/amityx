599a3e2644eedea22db6ee5bd90f64612fd85727
Aditya Tripathi <55402557+adityatrips@users.noreply.github.com>
2023-10-29 14:59:23 +0530

    Update redirect-login-btn.tsx

--- app/signup/redirect-login-btn.tsx
+++ app/signup/redirect-login-btn.tsx
@@ -1,13 +1,14 @@
 'use client';
 
-import { redirect } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const LoginBtn = () => {
+	const router = useRouter()
 	return (
 		<button
 			onClick={() => {
-				redirect('/login');
+				router.push('/login');
 			}}
 			className="w-full hover:bg-gray-800 px-4 py-2 rounded-xl flex itemsc-center justify-center gap-2"
 		>
